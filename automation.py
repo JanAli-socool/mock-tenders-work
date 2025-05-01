82113c59cf8ea63371f85f95a2c2c35346337342
Jan Ali Kazmi <99659205+JanAli-socool@users.noreply.github.com>
2025-05-02 01:59:59 +0500

    Update automation.py
    For Scheduler based working

--- automation.py
+++ automation.py
@@ -1,14 +1,13 @@
 # # run_daily.py
-# import schedule
-# import time
-# from extract_tender import extract_fields_from_tender
+import schedule
+import time
+from extract_tender import extract_fields_from_tender
 
-# def process_new_tenders():
-#     # simulate reprocessing from folder
-#     pass  # extend as needed
+def process_new_tenders():
+     # simulate reprocessing from folder
+     pass  # extend as needed
+ schedule.every().day.at("01:00").do(process_new_tenders)
 
-# schedule.every().day.at("01:00").do(process_new_tenders)
-
-# while True:
-#     schedule.run_pending()
-#     time.sleep(60)
+while True:
+     schedule.run_pending()
+     time.sleep(60)
